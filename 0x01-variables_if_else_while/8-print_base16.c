683cd7bf7a82462b7de0c1533f4bd42d49ad8a61
ogdmerlin <ogdmerlin@gmail.com>
2022-12-09 18:09:36 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+	char hexavalues[] = "0123456789abcdef";
+
+	for (i = 0; i< 16; i++)
+	{
+		putchar(hexavalues[i]);
+	}
+	putchar('\n');
+	return (0);
+}
