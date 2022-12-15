ed19dff64a84c55cee1c2fb52ae89a18ad4afe00
ogdmerlin <ogdmerlin@gmail.com>
2022-12-15 13:19:24 -0800

    Is upper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File - main.h
+ *
+ * Description: Header contains prototypes for all functions
+ * used in this project.
+ */
+
+int _putchar(char c);
+
+
+
+
+
+
+#endif
