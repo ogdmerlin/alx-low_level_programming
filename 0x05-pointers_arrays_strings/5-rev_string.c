54e9f523a3cdeb168172191c3fc5d8adb3b3f224
ogdmerlin <ogdmerlin@gmail.com>
2022-12-20 17:19:10 -0800

    Reverse String

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * rev_string - prints string in reverse
+ * @s: string to be reversed
+ *
+ * return: void
+ */
+
+void rev_string(char *s)
+{
+	int c, j, k, m;
+	char *r;
+
+	for (m = 0; m < 1; m++)
+	{
+		if (*s == '\0')
+			break;
+		j = 0;
+		while (*s != '\0')
+		{
+			*r = *s;
+			r++;
+			s++;
+			j++;
+		}
+		c = j;
+		while (j >= 0)
+		{
+			s--;
+			j--;
+		}
+		k = 0;
+		while (k <= c)
+		{
+			*s = *r;
+			s++;
+			r--;
+			k++;
+		}
+	}
+}
