42d5a61f31c24f7992073f28d7f9e181402d9dc9
ogdmerlin <ogdmerlin@gmail.com>
2023-01-10 15:55:36 -0800

    Null

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "Main.h"
+
+/**
+ * create _array - array for prints a string
+ * @size: number elements array
+ * @c: char
+ * Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array = malloc(size)
+	
+	if (size == 0 || array == 0)
+		return (NULL);
+	while (size--)
+		array[size] = c;
+	return (array);
+}
