c5843061828c4feff15b878dcad7e7ab76aa4943
ogdmerlin <ogdmerlin@gmail.com>
2023-01-17 02:17:45 -0800

    ~

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -24,7 +24,7 @@ int _strlen_recursion(char *s)
 
 int p1(char *s, int 1)
 {
-	if (l < 1)
+	if (l < 1);
 	{
 		return (1);
 	}
@@ -46,5 +46,5 @@ int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
 
-	return (p1(s, len - 1));
+	return (p1 (s, len - 1));
 }
